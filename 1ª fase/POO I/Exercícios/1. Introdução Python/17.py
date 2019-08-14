faf5d9fa43ff8f17a3e2e320cf538aeb43ebe890
Allan Soares <coallanvei@gmail.com>
2019-08-14 16:22:09 -0300

    ?

--- 1ª fase/POO I/Exercícios/1. Introdução Python/17.py
+++ 1ª fase/POO I/Exercícios/1. Introdução Python/17.py
@@ -3,7 +3,7 @@ P = int(input('Primeiro termo da equação: '))
 Op = input('Operação: ')
 Q = int(input('Segundo termo da equação: '))
 
-if N >= 1 and N <= 500000 and P >= 0 and P <= 1000 and Q >= 0 and Q <= 1000 :
+if N >= 1 and N <= 500000 and P >= 0 and P <= 1000 and Q >= 0 and Q <= 1000
     if Op == '+' and Q + P > N or Op == '*' and Q * P > N:
         print('OVERFLOW')
     elif Op == '+' or Op == '*':
