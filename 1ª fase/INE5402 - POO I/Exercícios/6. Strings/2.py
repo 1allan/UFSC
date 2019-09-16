2e6b460798a1de165e9985d4f54f2df221dc1b82
oyote <coallanvei@gmail.com>
2019-09-16 15:33:45 -0300

    bbbb

--- 1ª fase/INE5402 - POO I/Exercícios/6. Strings/2.py
+++ 1ª fase/INE5402 - POO I/Exercícios/6. Strings/2.py
@@ -1,18 +1,14 @@
 qnt = int(input('Quantidade de senhas: '))
 table = 'GQaku ISblv EOYcmw FPZnx JTeoy DNXfpz AKUgq CMWhr BLVis HRjt'
 table = table.split()
-res = ''
 
 for i in range(qnt):
     senha = input('Senha: ')
     cript = ''
 
-    if i == qnt - 1:
-        for j in range(len(senha)):
-            for k in range(len(table)):
-                if senha[j] in table[k]:
-                    cript += str(k)
+    for j in range(len(senha)):
+        for k in range(len(table)):
+            if senha[j] in table[k]:
+                cript += str(k)
 
-        res += cript[:12] + '\n'
-
-print(res)
+    print(cript[:12])
