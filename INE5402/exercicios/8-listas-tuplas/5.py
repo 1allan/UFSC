e52cf0a82da4c0b39ede71a7a53b8b089ecb572a
Allan <coallanvei@gmail.com>
2019-09-29 12:02:09 -0400

    "Você sabe o que todo mundo gosta?" Shrek, 00:27:46

--- INE5402/exercicios/8-listas-tuplas/5.py
+++ INE5402/exercicios/8-listas-tuplas/5.py
@@ -1,19 +1,14 @@
 numeros = []
 
- for i in range(5):
+for i in range(5):
      n = float(input())
 
      if n not in numeros:
          numeros.append(n)
     
-
-print(numeros)
-
 for i in range(len(numeros)):
     for j in range(len(numeros)):
         if numeros[j] > numeros[i]:
-            balde = numeros[i]
-            numeros[i] = numeros[j]
-            numeros[j] = balde
+            numeros[i], numeros[j] = numeros[j], numeros[i]
 
 print(numeros)
